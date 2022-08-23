7a066abdf0144c7dab4f5dd38f4b8facc92135f7
ndohertyjr <ndohertyjr@gmail.com>
2022-08-23 00:11:11 -0400

    Added movement, collision, and level reload

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,8 +5,11 @@ using UnityEngine.PlayerLoop;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] private float rocketSpeed = 10f;
-
+    [SerializeField] private float rocketThrust = 2000.0f;
+    [SerializeField] private float rotationSpeed = 100.0f;
+    private AudioSource rocketLaunchSound;
+    private bool audioPlaying = false;
+    private Rigidbody rocketBody;
     private float xVal;
     private float yVal;
     private float zVal;
@@ -14,7 +17,8 @@ public class Movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rocketBody = GetComponent<Rigidbody>();
+        rocketLaunchSound = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -28,20 +32,40 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
+            rocketBody.AddRelativeForce(new Vector3(0f,rocketThrust * Time.deltaTime,0f));
+
+            if (!rocketLaunchSound.isPlaying)
+            {
+                rocketLaunchSound.Play();
+            }
+            
             
         }
+        else
+        {
+            rocketLaunchSound.Stop();
+        }
+        
+        
     }
 
     private void ProcessRotation()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.D))
         {
-            
+            ApplyRotation(-rotationSpeed);
         }
 
-        else if (Input.GetKey(KeyCode.LeftArrow))
+        else if (Input.GetKey(KeyCode.A))
         {
-            
+            ApplyRotation(rotationSpeed);
         }
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        rocketBody.freezeRotation = true;
+        transform.Rotate(Vector3.back * rotationThisFrame * Time.deltaTime);
+        rocketBody.freezeRotation = false;
     }
 }
